97453bfe895d6ef7904ab6b2d2986d75fee827f5
Ivan Ramos <ivanramos.notdev@gmail.com>
2023-06-12 20:23:37 +0800

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./node_modules/flowbite/**/*.js",
+    "./views/**/*.ejs"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
