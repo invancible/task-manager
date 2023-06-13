7dad1cf462b0e7b0128ff76b9b38533e39f93aed
Ivan Ramos <ivanramos.notdev@gmail.com>
2023-06-13 10:52:54 +0800

    setup db connection, dependencies, and middleware

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
+// Nodejs
 const path = require("path");
 
+// External dependencies
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+
+// Internal dependencies
+require("dotenv").config();
 
 const app = express();
+const store = new MongoDBStore({
+  uri: process.env.DB_URL,
+  collection: "sessions",
+});
+const csrfProtection = csrf();
 
-app.listen(3000);
+// Setup
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+// Routes
+
+// Connection
+mongoose
+  .connect(process.env.DB_URL)
+  .then((result) => {
+    console.log("Connection Success!");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("Connection Failed!");
+  });
